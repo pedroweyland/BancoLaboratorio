a60ad3406675c7b760fb2d3ca8018ee4cb0da17f
pedroweyland <weylandpedro@gmail.com>
2024-05-24 16:27:58 -0300

    test: Primer test del proyecto
    Agregue dos test simples - testClienteSuccess, para ver si se crea bien un cliente y
    testClienteExistenteException, para ver si toma correctamente la excepction

--- src/test/java/ar/utn/frbb/tup/service/administracion/gestion/clientes/CrearCuentaTest.java
+++ src/test/java/ar/utn/frbb/tup/service/administracion/gestion/clientes/CrearCuentaTest.java
@@ -3,14 +3,14 @@ package ar.utn.frbb.tup.service.administracion.gestion.clientes;
 import ar.edu.utn.frbb.tup.model.Cliente;
 import ar.edu.utn.frbb.tup.model.TipoPersona;
 import ar.edu.utn.frbb.tup.model.exception.ClienteExistenteException;
+import ar.edu.utn.frbb.tup.model.exception.ClienteNoEncontradoException;
 import ar.edu.utn.frbb.tup.persistence.ClienteDao;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CrearCuentaTest {
 
@@ -23,7 +23,7 @@ public class CrearCuentaTest {
     }
 
     @Test
-    public void testClienteSuccess() throws ClienteExistenteException {
+    public void testClienteSuccess() throws ClienteExistenteException, ClienteNoEncontradoException {
         Cliente cliente = new Cliente();
 
         cliente.setNombre("Pepo");
@@ -42,9 +42,51 @@ public class CrearCuentaTest {
         assertNotNull(clienteDao.findCliente(cliente.getDni()));
         //Testeo si el cliente se ha creado correctamente en mi archivo
 
-        clienteDao.deleteCliente(cliente.getDni());
+        Cliente aux = clienteDao.deleteCliente(cliente.getDni());
 
         assertNull(clienteDao.findCliente(cliente.getDni()));
         //Testeo si el cliente se ha eliminado correctamente en mi archivo
+
+
+    }
+
+    @Test
+    public void testClienteExistenteException() throws ClienteExistenteException, ClienteNoEncontradoException {
+        Cliente cliente = new Cliente();
+
+        cliente.setNombre("Pepo");
+        cliente.setApellido("Weyland");
+        cliente.setDireccion("Berutti");
+        cliente.setDni(45501926);
+        cliente.setFechaNacimiento(LocalDate.of(2004, 1, 21));
+
+        cliente.setMail("weylandpedro@gmail.com");
+        cliente.setBanco("Macro");
+        cliente.setTipoPersona(TipoPersona.PERSONA_FISICA);
+        cliente.setFechaAlta(LocalDate.now());
+
+        clienteDao.saveCliente(cliente);
+
+        Cliente clienteNuevo = new Cliente();
+
+        clienteNuevo.setNombre("Carlitos");
+        clienteNuevo.setApellido("Weyland");
+        clienteNuevo.setDireccion("Berutti");
+        clienteNuevo.setDni(45501926);
+        clienteNuevo.setFechaNacimiento(LocalDate.of(2004, 1, 21));
+
+        clienteNuevo.setMail("Carlitos@gmail.com");
+        clienteNuevo.setBanco("Santader");
+        clienteNuevo.setTipoPersona(TipoPersona.PERSONA_FISICA);
+        clienteNuevo.setFechaAlta(LocalDate.now());
+
+        //El cliente ya existe por ende deberia lanzar una excepcion
+        assertThrows(ClienteExistenteException.class, () -> clienteDao.saveCliente(clienteNuevo));
+
+        //Elimino el cliente que fue guardado previamente
+        Cliente aux = clienteDao.deleteCliente(cliente.getDni());
+
+
+
     }
 }
